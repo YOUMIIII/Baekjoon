a0a54ee0e443df7f7c9e60108c87654f8f752ef9
YOUMIIII <youmi8255@naver.com>
2022-12-06 17:49:42 +0900

    백준 10871번 / X보다 작은 수

---
+++ Baekjoon/src/q10871/Main.java
@@ -0,0 +1,48 @@
+/*
+정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+<입력>
+첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+<출력>
+X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+<입력 예>
+10 5
+1 10 4 9 2 3 8 5 7 6
+<출력 예>
+1 4 2 3
+*/
+
+package q10871;
+
+import java.util.Scanner;
+
+public class Main {
+	private int n;
+	private int x;
+	int[] numarr;
+
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		x = sc.nextInt();
+		numarr = new int[n];
+		for (int i = 0; i < numarr.length; i++) {
+			numarr[i] = sc.nextInt();
+		}
+		sc.close();
+	}
+	
+	public void output() {
+		for (int i = 0; i < n; i++) {
+			if (numarr[i] < x) {
+				System.out.print(numarr[i] + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Main mm = new Main();
+		mm.input();
+		mm.output();
+	}
+}
